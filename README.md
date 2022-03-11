d4ef47924359c097e32a43bfff075897979fdaa7
Adrian Torchiana <adriantorchiana@granular.ag>
2022-03-11 14:56:35 -0800

    First pass at EM with Y MCAR

--- README.md
+++ README.md
@@ -1,5 +1,11 @@
 # semi_supervised_gmm
 
+Related:
+ - https://www.cs.ubc.ca/~schmidtm/Courses/540-W16/EM.pdf
+ - https://www.cs.cmu.edu/~epxing/Class/10708-15/notes/10708_scribe_lecture8.pdf
+
 ![True Y](plot_simulation_x_and_true_y.png)
 
 ![Observed Y](plot_simulation_x_and_observed_y.png)
+
+![Mean of X given Y](plot_mu_x_true_and_estimated.png)
--- simulation.py
+++ simulation.py
@@ -1,6 +1,104 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
+from scipy.stats import multivariate_normal
+
+
+def update_params_hat(df, pr_y_given_x):
+
+    n_classes = pr_y_given_x.shape[1]
+
+    params_hat = {
+        "n_classes": n_classes,
+        "pr_y": pr_y_given_x.mean(axis=0),
+        "mu_x": [],
+        "vcov_x": [],
+    }
+
+    for c in range(n_classes):
+
+        params_hat["mu_x"].append(
+            np.average(df[["x1", "x2"]], weights=pr_y_given_x[:, c], axis=0)
+        )
+
+        # TODO Make sure this is correct, check ddof
+        params_hat["vcov_x"].append(
+            np.cov(df[["x1", "x2"]], rowvar=False, aweights=pr_y_given_x[:, c])
+        )
+
+    return params_hat
+
+
+def calculate_pr_y_given_x(df, params_hat):
+
+    n_obs = df.shape[0]
+
+    # Could also get this from len(params_hat["pr_y"])
+    n_classes = params_hat["n_classes"]
+
+    multivariate_normals = [
+        multivariate_normal(mean=params_hat["mu_x"][c], cov=params_hat["vcov_x"][c])
+        for c in range(n_classes)
+    ]
+
+    pr_y_given_x = np.zeros((n_obs, n_classes))
+    for i in range(n_obs):
+        observed_class = df["observed_y"].iloc[i]
+
+        if not np.isnan(observed_class):
+
+            # We are absolutely certain of Y for this observation (it is observed)
+            pr_y_given_x[i, int(observed_class)] = 1.0
+
+        else:
+
+            # Y is not observed, we need to calculate Pr[ Y | X ]
+            observed_x = df[["x1", "x2"]].iloc[i]
+            priors_times_densities = np.zeros((n_classes,))
+            for c in range(n_classes):
+                priors_times_densities[c] = params_hat["pr_y"][
+                    c
+                ] * multivariate_normals[c].pdf(observed_x)
+
+            pr_y_given_x[i, :] = priors_times_densities / np.sum(priors_times_densities)
+
+    # Sanity check
+    assert np.isclose(pr_y_given_x.sum(), n_obs)
+
+    return pr_y_given_x
+
+
+def run_expectation_maximization(df, n_classes, max_iter=20):
+
+    # TODO Randomize initial values of mu_x?
+    # Try multiple random starting values, return best likelihood
+    # Note that [0, 0] and n=2 both hardcode the dimension of the predictors ["x1", "x2"]
+    params_hat = {
+        "pr_y": [
+            1 / n_classes,
+        ]
+        * n_classes,
+        "mu_x": [
+            [0, 0],
+        ]
+        * n_classes,
+        "vcov_x": [
+            np.identity(n=2),
+        ]
+        * n_classes,
+        "n_classes": n_classes,
+    }
+
+    for iter_idx in range(max_iter):
+        print(f"EM iteration {iter_idx}")
+
+        ## E step: update pr_y_given_x
+        pr_y_given_x = calculate_pr_y_given_x(df, params_hat)
+
+        ## M step: update params_hat
+        params_hat = update_params_hat(df, pr_y_given_x)
+
+    return params_hat
 
 
 def simulate_x(df, mu_x, vcov_x, n_classes):
@@ -52,10 +150,12 @@ def simulate(
 
 def main():
 
+    # TODO Set np random seed
+
     # Entry i is the mean of X conditional on Y==i
     # The predictors X are in R^2
     mu_x = [
-        [2.5, 1],
+        [4.5, 0.5],
         [-1, -3],
         [1, 1],
         [5, 4],
@@ -123,6 +223,41 @@ def main():
     ax.legend()
     plt.savefig("plot_simulation_x_and_observed_y.png")
     plt.clf()
+
+    # EM is not allowed to use true_y
+    # It only sees observed_y (which contains many NaNs)
+    params_hat = run_expectation_maximization(
+        df=df_train[["x1", "x2", "observed_y"]], n_classes=len(pr_y), max_iter=30
+    )
+
+    fig, ax = plt.subplots(figsize=(10, 8))
+    # TODO Could be fun to have a similar plot showing path of EM params_hat over time (iteration by iteration)
+    for c in range(params_hat["n_classes"]):
+        color = next(ax._get_lines.prop_cycler)["color"]
+        plt.plot(
+            mu_x[c][0],
+            mu_x[c][1],
+            marker="s",
+            linestyle="",
+            label=f"true E[ X | Y = {c}]",
+            color=color,
+        )
+        plt.plot(
+            params_hat["mu_x"][c][0],
+            params_hat["mu_x"][c][1],
+            marker="+",
+            linestyle="",
+            label=f"estimated E[ X | Y = {c}]",
+            color=color,
+        )
+
+    ax.legend()
+    plt.savefig("plot_mu_x_true_and_estimated.png")
+    plt.clf()
+
+    # TODO Also plot Pr[ Y | X ], or plot gaussian ellipses around E[ X | Y ] to visualize estimated vcov matrices
+
+    # TODO Evaluate model on a test set
 
 
 if __name__ == "__main__":
