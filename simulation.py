111672538b24ae24d9dd5d8bdbb3131e2bed2e39
Adrian Torchiana <adrian.torchiana@gmail.com>
2022-03-15 09:14:51 -0700

    Evaluate GMM on a test set (and compare to simple random forest baselines)

--- simulation.py
+++ simulation.py
@@ -2,6 +2,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 from scipy.stats import multivariate_normal
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import log_loss
 
 
 def update_params_hat(df, pr_y_given_x):
@@ -74,18 +76,9 @@ def run_expectation_maximization(df, n_classes, max_iter=20):
     # Try multiple random starting values, return best likelihood
     # Note that [0, 0] and n=2 both hardcode the dimension of the predictors ["x1", "x2"]
     params_hat = {
-        "pr_y": [
-            1 / n_classes,
-        ]
-        * n_classes,
-        "mu_x": [
-            [0, 0],
-        ]
-        * n_classes,
-        "vcov_x": [
-            np.identity(n=2),
-        ]
-        * n_classes,
+        "pr_y": [1 / n_classes,] * n_classes,
+        "mu_x": [[0, 0],] * n_classes,
+        "vcov_x": [np.identity(n=2),] * n_classes,
         "n_classes": n_classes,
     }
 
@@ -120,11 +113,7 @@ def simulate_x(df, mu_x, vcov_x, n_classes):
 
 
 def simulate(
-    n_obs,
-    pr_y,
-    mu_x,
-    vcov_x,
-    pr_y_is_observed,
+    n_obs, pr_y, mu_x, vcov_x, pr_y_is_observed,
 ):
 
     n_classes = len(pr_y)
@@ -148,64 +137,27 @@ def simulate(
     return df
 
 
-def main():
+def save_plots(df_train, params_hat, mu_x):
 
-    # TODO Set np random seed
-
-    # Entry i is the mean of X conditional on Y==i
-    # The predictors X are in R^2
-    mu_x = [
-        [4.5, 0.5],
-        [-1, -3],
-        [1, 1],
-        [5, 4],
-    ]
-
-    # Entry i is the variance-covariance matrix of X conditional on Y==i
-    # Make sure these are positive (semi-) definite
-    vcov_x = [
-        [
-            [2.0, -0.3],
-            [-0.3, 1.0],
-        ],
-        [
-            [2, 0],
-            [0, 3],
-        ],
-        [
-            [2, 1],
-            [1, 4],
-        ],
-        [
-            [3.0, 1.5],
-            [1.5, 2.0],
-        ],
-    ]
-
-    pr_y = [0.2, 0.3, 0.4, 0.1]
-    df_train = simulate(
-        n_obs=500,
-        pr_y=pr_y,
-        mu_x=mu_x,
-        vcov_x=vcov_x,
-        pr_y_is_observed=0.05,
-    )
+    n_classes = params_hat["n_classes"]
 
     fig, ax = plt.subplots(figsize=(10, 8))
     x1 = np.array(df_train["x1"])
     x2 = np.array(df_train["x2"])
-    for c in range(len(pr_y)):
+    for c in range(n_classes):
         idx_class = np.where(df_train["true_y"] == c)[0].tolist()
         plt.plot(
             x1[idx_class], x2[idx_class], label=f"Y = {c}", marker="o", linestyle=""
         )
 
     ax.legend()
+    plt.xlabel("x1")
+    plt.ylabel("x2")
     plt.savefig("plot_simulation_x_and_true_y.png")
     plt.clf()
 
     fig, ax = plt.subplots(figsize=(10, 8))
-    for c in range(len(pr_y)):
+    for c in range(n_classes):
         idx_class = np.where(df_train["observed_y"] == c)[0].tolist()
         plt.plot(
             x1[idx_class], x2[idx_class], label=f"Y = {c}", marker="o", linestyle=""
@@ -221,18 +173,14 @@ def main():
         alpha=0.5,
     )
     ax.legend()
+    plt.xlabel("x1")
+    plt.ylabel("x2")
     plt.savefig("plot_simulation_x_and_observed_y.png")
     plt.clf()
 
-    # EM is not allowed to use true_y
-    # It only sees observed_y (which contains many NaNs)
-    params_hat = run_expectation_maximization(
-        df=df_train[["x1", "x2", "observed_y"]], n_classes=len(pr_y), max_iter=30
-    )
-
     fig, ax = plt.subplots(figsize=(10, 8))
     # TODO Could be fun to have a similar plot showing path of EM params_hat over time (iteration by iteration)
-    for c in range(params_hat["n_classes"]):
+    for c in range(n_classes):
         color = next(ax._get_lines.prop_cycler)["color"]
         plt.plot(
             mu_x[c][0],
@@ -252,12 +200,88 @@ def main():
         )
 
     ax.legend()
+    plt.xlabel("x1")
+    plt.ylabel("x2")
     plt.savefig("plot_mu_x_true_and_estimated.png")
     plt.clf()
 
     # TODO Also plot Pr[ Y | X ], or plot gaussian ellipses around E[ X | Y ] to visualize estimated vcov matrices
+    return
 
-    # TODO Evaluate model on a test set
+
+def main():
+
+    # TODO Set np random seed
+
+    # Entry i is the mean of X conditional on Y==i
+    # The predictors X are in R^2
+    mu_x = [
+        [4.5, 0.5],
+        [-1, -3],
+        [1, 1],
+        [5, 4],
+    ]
+
+    # Entry i is the variance-covariance matrix of X conditional on Y==i
+    # Make sure these are positive (semi-) definite
+    vcov_x = [
+        [[2.0, -0.3], [-0.3, 1.0],],
+        [[2, 0], [0, 3],],
+        [[2, 1], [1, 4],],
+        [[3.0, 1.5], [1.5, 2.0],],
+    ]
+
+    pr_y = [0.2, 0.3, 0.4, 0.1]
+    df_train = simulate(
+        n_obs=500, pr_y=pr_y, mu_x=mu_x, vcov_x=vcov_x, pr_y_is_observed=0.05,
+    )
+
+    # EM is not allowed to use true_y
+    # It only sees observed_y (which contains many NaNs)
+    params_hat = run_expectation_maximization(
+        df=df_train[["x1", "x2", "observed_y"]], n_classes=len(pr_y), max_iter=30
+    )
+
+    save_plots(df_train, params_hat, mu_x)
+
+    # Fit a random forest that gets to observe _all_ Ys (i.e. true_y instead of observed_y)
+    rf_true_y = RandomForestClassifier(n_estimators=200, max_features="sqrt")
+    rf_true_y.fit(df_train[["x1", "x2"]], df_train["true_y"])
+
+    # Fit a random forest that gets to observe only observed_y (but doesn't make any use of the unlabeled Xs)
+    rf_observed_y = RandomForestClassifier(n_estimators=200, max_features="sqrt")
+    y_is_observed = df_train["observed_y"].notnull()
+    rf_observed_y.fit(
+        df_train.loc[y_is_observed, ["x1", "x2"]],
+        df_train.loc[y_is_observed, "observed_y"],
+    )
+
+    # We're going to evaluate three models on this test set
+    # (a) our GMM which was trained on df_train[["x1", "x2", "observed_y"]]
+    # (b) a random forest that gets to "cheat" and train on df_train[["x1", "x2", "true_y"]]
+    # (c) a random forest trained on the subset of df_train[["x1", "x2", "observed_y"]] where observed_y is truly observed (i.e. not null)
+    df_test = simulate(
+        n_obs=1000, pr_y=pr_y, mu_x=mu_x, vcov_x=vcov_x, pr_y_is_observed=0.0,
+    )
+
+    # This function uses the estimated GMM to predict Y | X
+    # as long as observed_y is NaN (note pr_y_is_observed=0.0 above)
+    predictions_gmm = calculate_pr_y_given_x(df_test, params_hat)
+
+    predictions_rf_true_y = rf_true_y.predict_proba(df_test[["x1", "x2"]])
+    predictions_rf_observed_y = rf_observed_y.predict_proba(df_test[["x1", "x2"]])
+
+    test_loss_gmm = log_loss(df_test["true_y"], y_pred=predictions_gmm)
+    test_loss_rf_true_y = log_loss(df_test["true_y"], y_pred=predictions_rf_true_y)
+    test_loss_rf_observed_y = log_loss(
+        df_test["true_y"], y_pred=predictions_rf_observed_y
+    )
+
+    # TODO Replicate with different df_train to get sampling distribution of test_loss_* vars
+    print("Test set log losses (lower is better):")
+    print(f"test_loss_gmm = {test_loss_gmm}")
+    print(f"test_loss_rf_true_y = {test_loss_rf_true_y}")
+    print(f"test_loss_rf_observed_y = {test_loss_rf_observed_y}")
 
 
 if __name__ == "__main__":
